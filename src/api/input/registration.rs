771288da8c9f5a230d91c93c57a1bfe8408b93c5
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-01-24 00:56:25 +0200

    + register 'user' input api element

--- src/api/input/registration.rs
+++ src/api/input/registration.rs
@@ -5,7 +5,8 @@ use cooplan_amqp_api::error::Error;
 use cooplan_lapin_wrapper::config::api::Api;
 
 pub fn register(api: &Api) -> Result<Vec<InputElement<LogicRequest>>, Error> {
-    let elements: Vec<InputElement<LogicRequest>> = vec![elements::organization::get(api)?];
+    let elements: Vec<InputElement<LogicRequest>> =
+        vec![elements::organization::get(api)?, elements::user::get(api)?];
 
     Ok(elements)
 }
